1de426c1aca646d03a51e9e064b58b37b7bfd5dc
Ihsaan Patel <ihsaan.patel@gmail.com>
2017-11-25 12:01:28 -0600

    added enum name for postgres

--- SharingEconomy/models.py
+++ SharingEconomy/models.py
@@ -5,8 +5,8 @@ class Vote(db.Model):
     __tablename__ = 'votes'
 
     id = db.Column(db.Integer, primary_key = True, nullable = False)
-    stakeholder = db.Column(db.Enum('uber', 'taxi', 'consumer', 'worker'), nullable = False)
+    stakeholder = db.Column(db.Enum('uber', 'taxi', 'consumer', 'worker', name='stakeholders'), nullable = False)
     date_added = db.Column(db.DateTime, default=datetime.datetime.today())
-    vote = db.Column(db.Enum('Win', 'Lose'), nullable = False)
+    vote = db.Column(db.Enum('Win', 'Lose', name = 'vote_tag'), nullable = False)
 
     
