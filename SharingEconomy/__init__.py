9221b2dd82ffb17b090aa150faad91c5f7f7ce03
Ihsaan Patel <ihsaan.patel@gmail.com>
2017-11-25 11:59:54 -0600

    removed unnecessary logging code

--- SharingEconomy/__init__.py
+++ SharingEconomy/__init__.py
@@ -8,16 +8,6 @@ db = SQLAlchemy(app)
 db.create_all()
 import SharingEconomy.views
 
-if not app.debug and os.environ.get('HEROKU') is None:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/sharing_economy.log', 'a', 1 * 1024 * 1024, 10)
-    file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    app.logger.addHandler(file_handler)
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('sharing_economy startup')
-
 if os.environ.get('HEROKU') is not None:
     import logging
     stream_handler = logging.StreamHandler()
